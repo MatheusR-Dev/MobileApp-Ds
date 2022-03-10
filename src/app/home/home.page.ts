cbae252540d4e26b7f28232c750092b51d26fbaf
unknown <matheusfelipelr@gmail.com>
2022-03-09 22:51:38 -0300

    alterações

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  dado: number = 0;
+  sorteado: boolean = false;
   constructor() {}
+
+  async sortear() {
+    for (let i = 0 ; i < 10; i++){
+    this.sorteado = true;
+    const numSorteado = Math.random();
+    this.dado = Math.floor(numSorteado * 6 + 1);
+    await new Promise(f => setTimeout(f, 200));
+    }
+    this.sorteado = false;
+  }
 
 }
